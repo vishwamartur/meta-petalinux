c5be82fc1e3053eafa1a33bd83bd19bf1afb76f8
Bingi Vamshi Krishna <vamshi.krishna.bingi@amd.com>
2024-02-22 23:44:11 +0530

    resize-partition.sh: Correted the usage of resize-part command
    corrected the usage of resize-part command and updating partition number to 2 in auto startup script for resize
    
    Signed-off-by: Bingi Vamshi Krishna <vamshi.krishna.bingi@amd.com>
    Signed-off-by: Mark Hatle <mark.hatle@amd.com>
    (cherry picked from commit 98f74c2b6e067f82a624b4aee06bafa029ad5c9b)
    Signed-off-by: Mark Hatle <mark.hatle@amd.com>

--- recipes-apps/resize-partition/files/resize-partition.sh
+++ recipes-apps/resize-partition/files/resize-partition.sh
@@ -2,9 +2,9 @@
 
 # Expand the sd card partition to 100% from unpartitioned space.
 #
-# Usage: resize-part "/dev/mmcblk0p2"
+# Usage: resize-part "/dev/mmcblk0 p2"
 
-partition="/dev/mmcblk0p2"
+partition="/dev/mmcblk0 p2"
 
 echo "Expanding the Partition:${partition} to 100%"
 /usr/bin/resize-part ${partition}
